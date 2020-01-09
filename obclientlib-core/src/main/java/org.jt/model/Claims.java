4098d5327aa7374917258afab96ce12a7082b70a
JT <jamestasker321@gmail.com>
2020-01-09 21:44:49 +0000

    Jan 2020 fixes

--- obclientlib-core/src/main/java/org.jt/model/Claims.java
+++
@@ -1,9 +0,0 @@
-package org.jt.model;
-
-/**
- * Created by david on 09/06/2019.
- */
-public class Claims {
-
-    public final static String OPEN_BANKING_INTENT_ID_CLAIM="openbanking_intent_id";
-}
